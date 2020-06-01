7188de6e553cdcfc6e6c5ef23a65003b8917dddc
Cloud User <centos@ip-172-31-0-162.eu-west-2.compute.internal>
2020-06-01 10:38:20 +0000

    FinalEx1

--- Ex1/top.v
+++ Ex1/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #1 
-// Student Name:
-// Date: 
+// Student Name: ISIDORA RADENKOVIC
+// Date: June 1st, 2020
 //
 //  Description: In this exercise, you need to design a logical unit, where the  
 //  output acts according to the following truth table:
