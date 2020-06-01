2a1215d80ef4c16fa739826d2d2a342803fd735b
Cloud User <centos@ip-172-31-0-162.eu-west-2.compute.internal>
2020-06-01 13:56:09 +0000

    AttemptEx3

--- Ex3/top_tb.v
+++ Ex3/top_tb.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #3 - Up/Down counter
-// Student Name:
-// Date: 
+// Student Name:ISIDORA RADENKOVIC
+// Date: June 1st, 2020
 //
 // Description: A testbench module to test Ex3 - counter
 // Guidance: start with simple tests of the module (how should it react to each 
@@ -14,15 +14,30 @@ module top_tb(
     );
     
 //Todo: Parameters
+	parameter CLK_PERIOD = 10;
 
 //Todo: Regitsers and wires
+	reg [7:0] counter_out;
+	reg err;
 
 //Todo: Clock generation
+
+initial
+    begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+     end
 
 //Todo: User logic
     
 //Todo: Finish test, check for success
-
+initial begin
+        #50 
+        if (err==0)
+          $display("***TEST PASSED! :) ***");
+        $finish;
+      end
 //Todo: Instantiate counter module
  
 endmodule 
