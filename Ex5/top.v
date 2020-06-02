e3c8e368ed3ac7857b75727402aea1a87989a5f2
Cloud User <centos@ip-172-31-0-162.eu-west-2.compute.internal>
2020-06-02 15:54:03 +0000

    FixEx5

--- Ex5/top.v
+++ Ex5/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #5 
-// Student Name:
-// Date: 
+// Student Name:ISIDORA RADENKOVIC
+// Date: June 2nd, 2020
 //
 //  Description: In this exercise, you need to implement a UK traffic lights 
 //  sequencing system. 
@@ -12,4 +12,67 @@
 //  outputs:
 //           red, amber, green
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns/100ps
 
+module traffic_lights(
+	input clk,
+	output red,
+	output amber,
+	output green
+	
+	);
+
+//This counter is connected to the lights in the following order:
+// red, red+amber, green, amber -->  00, 01, 10, 11
+	reg [1:0]lights;
+
+
+assign red = (lights[1])?(1'b0):(1'b1);
+assign amber = (lights[0])?(1'b1):(1'b0);
+assign green = (lights == 2'b10)? (1'b1):(1'b0);
+
+
+always @(posedge clk)
+begin
+
+if (lights==2'b11)
+lights <= 2'b0;
+else 
+lights <= lights+1;
+
+/*
+if (lights==2'b0)
+	begin
+	red <= 1;
+	amber <= 0;
+	green <= 0;
+	end
+else 
+begin
+	if (lights==2'b01)
+		begin
+		red <= 1;
+		amber <= 1;
+		green <= 0;
+		end
+	else 
+	begin
+		if(lights==2'b10)
+			begin
+			red <= 0;
+			amber <= 0;
+			green <= 1;
+			end
+		else
+		begin
+			if(lights==2'b11)
+			red <= 0;
+			amber <= 1;
+			green <= 0;
+		end
+	end
+end
+*/
+end
+
+endmodule
