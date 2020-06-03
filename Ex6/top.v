3d2ae39f16be556d55ad72baa68b1489ce1bdc78
Cloud User <centos@ip-172-31-0-162.eu-west-2.compute.internal>
2020-06-03 14:43:00 +0000

    AttemptEx6

--- Ex6/top.v
+++ Ex6/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #6 
-// Student Name:
-// Date: 
+// Student Name:ISIDORA RADENKOVIC
+// Date: June 3rd, 2020
 //
 //
 //  Description: In this exercise, you need to design a multiplexer between a dice and traffic 
@@ -19,4 +19,26 @@
 //           result[2:0]
 //
 //////////////////////////////////////////////////////////////////////////////////
+
+`timescale 1ns/100ps
+
+module MuxDiceTraffic(
+input rst,
+input clk,
+input button,
+input sel,
+output wire [2:0] result
+);
+
+wire [2:0] tf;
+wire [2:0] di;
+
+
+
+traffic_lights top1(clk, tf[2], tf[1], tf[0]);
+dice top2(clk, rst, button, di);
+assign result = (sel)?(tf):(di);
+
+endmodule
+
 
